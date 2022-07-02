e8cbf7f22a53b069ec23a9a8e786ce512a484c84
superDude109 <superdude@Bobs-Mouth.localdomain>
2022-07-02 00:29:41 -0500

    Trying anything here

--- ui/src/index.js
+++ ui/src/index.js
@@ -6,9 +6,11 @@ import reportWebVitals from './reportWebVitals';
 
 
 ReactDOM.render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+  <React.StrictMode>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>  
+</React.StrictMode>
   ,
   document.getElementById('root')
 )
