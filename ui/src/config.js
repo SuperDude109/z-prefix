b3ccb46548784bb15fe6dc043dc215aeea4b4c1a
superDude109 <superdude@Bobs-Mouth.localdomain>
2022-07-04 13:19:06 -0500

    front end attached to new backend

--- ui/src/config.js
+++ ui/src/config.js
@@ -5,7 +5,7 @@ const config = {
     },
 
     production: {
-        apiUrl: "https://zpre-frontend.herokuapp.com/"
+        apiUrl: "https://zprebackup.herokuapp.com"
     },
     test: {
         apiUrl: ''
